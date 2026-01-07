02ac93a9e606d6d3a9fc572a5397df57ae01d87b
Sky Margate <skymargate@gmail.com>
2026-01-07 14:35:26 +0800

    changeed layout

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,15 @@
 export const metadata = {
   title: "The Cozy Corner",
   description: "A cozy online bookstore with hand-picked recommendations",
-    verification: {
-        google:'a3uFpKLX9_5aq68QqiBihUmYbSA_YYTM414ZNSeBB2Q'
-    },
+  verification: {
+    google: 'a3uFpKLX9_5aq68QqiBihUmYbSA_YYTM414ZNSeBB2Q',
+  },
 };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
