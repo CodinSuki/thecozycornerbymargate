b7d068ec712553049300164a25d38ba004d32196
Sky Margate <skymargate@gmail.com>
2026-01-07 14:44:13 +0800

    pleas eword

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,4 @@
-import './globals.css'
-
 export const metadata = {
   title: "The Cozy Corner",
   description: "A cozy online bookstore with hand-picked recommendations",
-  verification: {
-    google: 'a3uFpKLX9_5aq68QqiBihUmYbSA_YYTM414ZNSeBB2Q',
-  },
 };
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
