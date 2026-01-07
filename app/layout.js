4295b523bd0780e282887d39462a32e7443ce4dd
Sky Margate <skymargate@gmail.com>
2026-01-07 14:22:50 +0800

    yes

--- app/layout.js
+++ app/layout.js
@@ -2,4 +2,7 @@
 export const metadata = {
   title: "The Cozy Corner",
   description: "A cozy online bookstore with hand-picked recommendations",
+    verification: {
+        google:'a3uFpKLX9_5aq68QqiBihUmYbSA_YYTM414ZNSeBB2Q'
+    },
 };
