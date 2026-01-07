6296e99f15150f5059507d952e3858b6ac4024fd
Sky Margate <skymargate@gmail.com>
2026-01-07 14:11:05 +0800

    google verift

---
+++ app/layout.js
@@ -0,0 +1,8 @@
+<head>
+  <meta name="google-site-verification" content="googleaefebfb61622ac00.html" />
+</head>
+
+export const metadata = {
+  title: "The Cozy Corner",
+  description: "A cozy online bookstore with hand-picked recommendations",
+};
