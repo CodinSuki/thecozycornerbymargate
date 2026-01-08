1aefba781c35198ba2851e6e7c523d37d5a94e23
Sky Margate <skymargate@gmail.com>
2026-01-08 13:17:44 +0800

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,7 +2,7 @@ export default function AboutPage() {
   return (
     <section classname ="bg-black"> 
       <h2 className="text-3xl font-semibold mb-4">About The Cozy Corner</h2>
-      <p className="text-zinc-700 max-w-2xl ">
+      <p className="text-white max-w-2xl ">
         The Cozy Corner is an online bookstore built for readers who enjoy calm,
         comfort, and meaningful stories. Our goal is to recommend books that
         inspire and relax.
